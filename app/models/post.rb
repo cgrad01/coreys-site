8593845dae06ac60aa82201e704c8428ef704f03
cgrad01 <cgrad01@gmail.com>
2016-01-17 17:30:28 -0500

    add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :admin
+
+  validates :title, :content, :category, :date, :admin_id, presence: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
   belongs_to :admin
+
+  validates :title, :description, :url, :github, :date, :admin_id, presence: true
 end
