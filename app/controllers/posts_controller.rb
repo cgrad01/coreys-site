43cc57d49eb0badab5584d3bbc9e2b8edd0c1150
cgrad01 <cgrad01@gmail.com>
2016-01-23 16:49:01 -0500

    add 'create' path for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,22 @@
 class PostsController < ApplicationController
   def new
   end
+
+  def create
+    @admin = find_admin
+    @post = @admin.posts.new(post_params)
+      if @post.save
+        redirect_to portfolio_path
+      else
+        @errors = @post.errors.full_messages
+        render :new
+      end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :content, :category, :date)
+    end
+
 end
