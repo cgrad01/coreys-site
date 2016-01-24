0d50f0faa3eb90d5002fa967ae7219dea9bab0cb
cgrad01 <cgrad01@gmail.com>
2016-01-24 16:19:45 -0500

    add filter posts method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ActiveRecord::Base
   belongs_to :admin
 
+  def self.filter_posts(category)
+    result = []
+    self.all.each do |post|
+      if post.category == category
+        result << post
+      end
+    end
+    result
+  end
+
   validates :title, :content, :category, :date, :admin_id, presence: true
 end
