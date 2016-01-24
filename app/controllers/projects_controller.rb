50efcaf41dcf02fdbe5b4a8caebf648aeb729325
cgrad01 <cgrad01@gmail.com>
2016-01-23 20:01:14 -0500

    add create projects path

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,21 @@
 class ProjectsController < ApplicationController
+    def new
+    end
+
+    def create
+    @admin = find_admin
+    @project = @admin.projects.new(project_params)
+      if @project.save
+        redirect_to portfolio_path
+      else
+        @errors = @project.errors.full_messages
+        render :new
+      end
+  end
+
+  private
+
+    def project_params
+      params.require(:project).permit(:title, :description, :url, :github, :date)
+    end
 end
