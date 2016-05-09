a34600b0bcce88dbd89aeb406a885705fd09fcff
cgrad01 <cgrad01@gmail.com>
2016-05-08 20:44:09 -0400

    fix scrollspy offset

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
-  $('body').scrollspy({ target: '#coreynav' })
+  var navOffset = $('#coreynav').height();
+
+  $('.navbar li a').click(function(event){
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    scrollBy(0, -navOffset);
+    $('navbar li').removeClass("active");
+    $(this).parent().addClass("active");
+  });
+
+  $('body').scrollspy({
+    target: '#coreynav',
+    offset: navOffset + 1
+ })
 
 });
+
+
+
