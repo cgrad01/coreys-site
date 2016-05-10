d9406606f5dbff7145f95db6cec5e36ff6aa5ef5
cgrad01 <cgrad01@gmail.com>
2016-05-10 13:45:38 -0400

    fix scrollspy with css, nix javascript

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -2,18 +2,10 @@ $(document).ready(function(){
 
   var navOffset = $('#coreynav').height();
 
-  $('.navbar li a').click(function(event){
-    event.preventDefault();
-    $($(this).attr('href'))[0].scrollIntoView();
-    scrollBy(0, -navOffset);
-    $('navbar li').removeClass("active");
-    $(this).parent().addClass("active");
-  });
-
   $('body').scrollspy({
     target: '#coreynav',
     offset: navOffset + 1
- })
+  });
 
 });
 
